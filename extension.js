f445010fcfebcbc7e28cdbef7b580576975ec70e
Peter Simonyi <pts@petersimonyi.ca>
2017-12-13 17:41:48 -0500

    Re-enable switch-workspace signal listener

--- extension.js
+++ extension.js
@@ -23,14 +23,12 @@ function enable() {
         return old_getAlwaysZoomOut.call(this);
     }
 
-    /*
     let slider = Main.overview._controls._thumbnailsSlider;
     listenerId = global.window_manager.connect('switch-workspace',
         Lang.bind(slider, slider._updateSlide));
-        */
 }
 
 function disable() {
     ThumbnailsSlider.prototype._getAlwaysZoomOut = old_getAlwaysZoomOut;
-    //global.window_manager.disconnect(listenerId);
+    global.window_manager.disconnect(listenerId);
 }
