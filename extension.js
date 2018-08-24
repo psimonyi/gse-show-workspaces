e1ce93f2f8065f3b3779a3a3ebe67465e8831c40
smac89 <smac89@users.noreply.github.com>
2018-08-24 15:03:05 -0600

    Redo plugin to use es6 syntax

--- extension.js
+++ extension.js
@@ -6,30 +6,61 @@ const Main = imports.ui.main;
 const Meta = imports.gi.Meta;
 const ThumbnailsSlider = imports.ui.overviewControls.ThumbnailsSlider;
 
-let old_getAlwaysZoomOut;
-let listenerId;
+class ShowWorkSpaces {
+    constructor({uuid="show-workspaces@uuid.com", name="Show Workspaces"} = {}) {
+        this._getAlwaysZoomOut = undefined;
+        this.listenerId = undefined;
+        this.uuid = uuid;
 
-function init() {
+        this.log(`Created "${name}"`);
+    }
+
+    start() {
+        const self = this;
+        self.log("Starting plugin...");
+        self._getAlwaysZoomOut = ThumbnailsSlider.prototype._getAlwaysZoomOut;
+        ThumbnailsSlider.prototype._getAlwaysZoomOut = function() {
+            let screen = global.screen;
+            // Always show the switcher if workspaces are in use.
+            if (!Meta.prefs_get_dynamic_workspaces() ||
+                screen.n_workspaces > 1 ||
+                screen.get_active_workspace_index() !== 0) {
+                return true;
+            }
+            // Also show it under the normal conditions.
+            return self._getAlwaysZoomOut.call(this);
+        };
+
+        let slider = Main.overview._controls._thumbnailsSlider;
+        self.listenerId = global.window_manager.connect('switch-workspace', 
+            Lang.bind(slider, slider._updateSlide));
+    }
+
+    stop() {
+        this.log("Stopping plugin...");
+        ThumbnailsSlider.prototype._getAlwaysZoomOut = this._getAlwaysZoomOut;
+        global.window_manager.disconnect(this.listenerId);
+    }
+
+    log(...message) {
+        global.log(`[${this.uuid}]: ${message.join(" ")}`);
+    }
+}
+
+var showWorkSpaces = undefined;
+
+function init(meta) {
+    showWorkSpaces = new ShowWorkSpaces(meta);
 }
 
 function enable() {
-    old_getAlwaysZoomOut = ThumbnailsSlider.prototype._getAlwaysZoomOut;
-    ThumbnailsSlider.prototype._getAlwaysZoomOut = function () {
-        // Always show the switcher if workspaces are in use.
-        let usingWorkspaces = !Meta.prefs_get_dynamic_workspaces() ||
-                              global.screen.n_workspaces > 2 ||
-                              global.screen.get_active_workspace_index() != 0;
-        if (usingWorkspaces) return true;
-        // Also show it under the normal conditions.
-        return old_getAlwaysZoomOut.call(this);
+    if (showWorkSpaces !== undefined) {
+        showWorkSpaces.start();   
     }
-
-    let slider = Main.overview._controls._thumbnailsSlider;
-    listenerId = global.window_manager.connect('switch-workspace',
-        Lang.bind(slider, slider._updateSlide));
 }
 
 function disable() {
-    ThumbnailsSlider.prototype._getAlwaysZoomOut = old_getAlwaysZoomOut;
-    global.window_manager.disconnect(listenerId);
+    if (showWorkSpaces !== undefined) {
+        showWorkSpaces.stop();
+    }
 }
