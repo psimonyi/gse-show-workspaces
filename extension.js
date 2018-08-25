bd9e625cf8f16d482cb5d54d2bda476c2b9e0c10
smac89 <smac89@users.noreply.github.com>
2018-08-24 21:32:58 -0600

    We can now pass in different slider controls to be hooked unto

--- extension.js
+++ extension.js
@@ -1,45 +1,89 @@
-// Basically revert this commit:
-// https://git.gnome.org/browse/gnome-shell/commit/js/ui/overviewControls.js?id=2d849759c837ebc60f41022ce9ae83616ba0274e
-
-const Lang = imports.lang;
 const Main = imports.ui.main;
-const Meta = imports.gi.Meta;
+const ExtensionSystem = imports.ui.extensionSystem;
+const SlidingControl = imports.ui.overviewControls.SlidingControl;
 const ThumbnailsSlider = imports.ui.overviewControls.ThumbnailsSlider;
+
+const Meta = imports.gi.Meta;
+
+const Config = imports.misc.config;
 
 class ShowWorkSpaces {
     constructor({uuid="show-workspaces@uuid.com", name="Show Workspaces"} = {}) {
-        this._getAlwaysZoomOut = undefined;
-        this.listenerId = undefined;
+        this.sliders = [];
         this.uuid = uuid;
 
+        // this.log(`Enabled extensions: ${[...new Set(ExtensionSystem.getEnabledExtensions())]}`);
         this.log(`Created "${name}"`);
+        this.log(`${JSON.stringify(Config.PACKAGE_NAME)}`);
     }
 
     start() {
-        const self = this;
-        self.log("Starting plugin...");
-        self._getAlwaysZoomOut = ThumbnailsSlider.prototype._getAlwaysZoomOut;
-        ThumbnailsSlider.prototype._getAlwaysZoomOut = function() {
-            let screen = global.screen;
-            // Always show the switcher if workspaces are in use.
-            if (!Meta.prefs_get_dynamic_workspaces() ||
-                screen.n_workspaces > 1 ||
-                screen.get_active_workspace_index() !== 0) {
-                return true;
-            }
-            // Also show it under the normal conditions.
-            return self._getAlwaysZoomOut.call(this);
-        };
+        this.log("Starting plugin...");
+        this.startForSlider(ThumbnailsSlider);
 
-        let slider = Main.overview._controls._thumbnailsSlider;
-        self.listenerId = global.window_manager.connect('switch-workspace', 
-            Lang.bind(slider, slider._updateSlide));
+        Main.overview.connect('showing', () => {
+            
+            // let workSpaceViews = Main.overview.viewSelector._workspacesDisplay._workspacesViews;
+            // this.log(`Number of views: ${workSpaceViews.length}`);
+            // let shown = false;
+            // for (const view of workSpaceViews) {
+            //     this.log(`WorkspaceView on monitor: ${view._monitorIndex}`,
+            //         `has ${view._workspaces.length} workspaces`);
+            //     // if (!shown) {
+            //     //     for (const workspace of view._workspaces) {
+            //     //         this.log(Object.getOwnPropertyNames(workspace.metaWorkspace));
+            //     //         break;
+            //     //     }
+            //     //     shown = true;
+            //     // }
+            // }
+
+            // this.log(`Controls: ${Object.getOwnPropertyNames(Main.overview._controls)}`)
+
+            // Main.overview._controls._thumbnailsSlider.slideIn();
+        });
     }
 
     stop() {
         this.log("Stopping plugin...");
-        ThumbnailsSlider.prototype._getAlwaysZoomOut = this._getAlwaysZoomOut;
-        global.window_manager.disconnect(this.listenerId);
+        for (const {source, initialSlide} of this.sliders) {
+            this.undoAlwaysShowSlider(source, initialSlide);
+        }
+    }
+
+    startForSlider(slidingControlType) {
+        if (slidingControlType.prototype instanceof SlidingControl) {
+            const _getSlide = slidingControlType.prototype._getSlide;
+
+            this.makeAlwaysShowSlider(slidingControlType, () => {
+                const screen = global.screen;
+                if (!Meta.prefs_get_dynamic_workspaces() ||
+                    screen.n_workspaces > 1 ||
+                    screen.get_active_workspace_index !== 0) {
+                    return 1;
+                }
+                return 0;
+            }, _getSlide);
+
+            this.sliders.push({
+                source: slidingControlType,
+                initialSlide: _getSlide
+            });
+        }
+    }
+
+    makeAlwaysShowSlider(slidingControlType, replaceGetSlide, fallback) {
+        slidingControlType.prototype._getSlide = function () {
+            let value = replaceGetSlide();
+            if (value !== 0) {
+                return value;
+            }
+            return fallback.call(this);
+        }
+    }
+
+    undoAlwaysShowSlider(slidingControlType, initialFunc) {
+        slidingControlType.prototype._getSlide = initialFunc;
     }
 
     log(...message) {
